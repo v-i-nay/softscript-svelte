35699baf5bac0198c33271b679a0fbb5edaf0c67
v-i-nay <vinay@softscrips.net>
2025-03-01 16:52:05 +0530

    make it ready

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  let artists = [];
+  console.log("Sanity API URL:", import.meta.env.VITE_SANITY_API_URL);
+
+  // Fetch data when the component mounts
+  onMount(async () => {
+    const response = await fetch(import.meta.env.VITE_SANITY_API_URL);
+    const data = await response.json();
+    artists = data.result; // Store artists in the variable
+  });
+</script>
+
+<h1>Artists List</h1>
+<ul>
+  {#each artists as artist}
+  
+    <li>{artist.name}</li>
+  {/each}
+</ul>
